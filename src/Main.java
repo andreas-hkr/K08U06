363ee5fbdcf00e3eb9ea2f331372e53621e245a0
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 17:17:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en metod som returnerar vad klockan är och använd den
+        // för att skriva ut: "Klockan är: tt:mm:ss"
+
+
+    }
+}
