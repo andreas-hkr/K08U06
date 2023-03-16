2d8c1fe80b3e008a20a7f09e2b9bf3af0fef9571
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 17:23:15 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Calendar;
+
 public class Main {
+    static String time() {
+        String time = Calendar.getInstance().getTime().toString();
+
+        return time.substring(11, 19);
+    }
+
     public static void main(String[] args) {
         // Skapa en metod som returnerar vad klockan är och använd den
         // för att skriva ut: "Klockan är: tt:mm:ss"
 
-
+        System.out.println("Klockan är: " + time());
     }
 }
